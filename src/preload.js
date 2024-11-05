8325ebdb97a1774097027d626532796c30e74d1b
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-11-04 22:47:53 -0500

    round trip communication between backend and frontend

--- src/preload.js
+++ src/preload.js
@@ -3,4 +3,14 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("electronUtils", {
   navButtonClicked: (buttonName) =>
     ipcRenderer.send("navButtonClicked", buttonName),
+  // From frontend to backend.
+  send: (channel, args) => {
+    ipcRenderer.send(channel, args);
+  },
+  // From backend to frontend.
+  receive: (channel, listener) => {
+    ipcRenderer.on(channel, (event, ...args) => listener(...args));
+  },
+  // From backend to frontend and back again.
+  invoke: (channel, args) => ipcRenderer.invoke(channel, args),
 });
--- src/removeApps.js
+++ src/removeApps.js
@@ -1,34 +1,74 @@
-const { BrowserView } = require("electron");
+const { BrowserView, ipcMain } = require("electron");
 const path = require("path");
+const { spawnSync } = require("child_process");
 
 function renderRemoveAppsPage(browserWindow, show = false) {
-  const removeApps = new BrowserView({
+  const removeAppsPage = new BrowserView({
     webPreferences: {
       contextIsolation: true,
       nodeIntegration: false,
       preload: path.join(__dirname, "./preload.js"),
     },
   });
-  removeApps.webContents.loadFile(
+  removeAppsPage.webContents.loadFile(
     path.join(__dirname, "../static/removeApps.html")
   );
 
-  removeApps.setBounds({
+  removeAppsPage.setBounds({
     x: 0,
     y: 36,
     width: browserWindow.getBounds().width,
     height: browserWindow.getBounds().height - 36,
   });
-  removeApps.setAutoResize({
+  removeAppsPage.setAutoResize({
     width: true,
     height: true,
     horizontal: false,
     vertical: false,
   });
 
-  if (show) browserWindow.addBrowserView(removeApps);
+  if (show) browserWindow.addBrowserView(removeAppsPage);
 
-  return removeApps;
+  ipcMain.addListener("refreshAppList", () => {
+    const flatpakList = runFlatpakList();
+    removeAppsPage.webContents.send("appList", flatpakList);
+  });
+
+  return removeAppsPage;
+}
+
+function parseFlatpakList(output) {
+  const lines = output.split("\n");
+
+  const dataLines = lines.filter((line) => line?.trim() !== "");
+
+  return dataLines.map((line) => {
+    const [name, appId, version, branch, installType] = line
+      .split("\t")
+      .map((p) => p?.trim());
+
+    return {
+      name,
+      appId,
+      version,
+      branch,
+      installType,
+    };
+  });
+}
+
+function runFlatpakList() {
+  let apps = [];
+
+  try {
+    const result = spawnSync("flatpak", ["list", "--app"]);
+
+    apps = parseFlatpakList(result.stdout.toString());
+  } catch (e) {
+    console.error("Error while fetching flatpak list: ", e);
+  }
+
+  return apps;
 }
 
 module.exports = {
